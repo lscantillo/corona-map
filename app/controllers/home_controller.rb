a5999049fd0e1fda4ed42dd8d454a618a5480c3a
Luis Sebastian Cantillo Zambrano <macproluis@MacBook-Pro-de-Luis.local>
2020-06-06 14:03:31 -0500

    skip token

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
-
+    skip_before_action :verify_authenticity_token
     def index
         @data = CoronaDatum.pluck(:country, :confirmed_cases)
     end
