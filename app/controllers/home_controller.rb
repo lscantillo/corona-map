8ba100df6771bc4635e843fc4975feb0097e9b30
Luis Sebastian Cantillo Zambrano <macproluis@MacBook-Pro-de-Luis.local>
2020-06-06 11:47:35 -0500

    First commit Coronavirus map

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,14 @@
+class HomeController < ApplicationController
+
+    def index
+      @data = CoronaDatum.pluck(:country, :confirmed_cases)
+    end
+
+    def receive_data
+        data = request.body.receive_data
+        JSON.parse(data)
+        data['Countries'].each do |hash| CoronaDatum.find_by(country: hash['Country']).update(confirmed_cases: hash['TotalConfirmed']) end
+            
+    end
+    
+end
