ba2b7674ba7a177f087d441fc103d41309cc7197
Luis Sebastian Cantillo Zambrano <macproluis@MacBook-Pro-de-Luis.local>
2020-06-06 13:52:14 -0500

    receive data from covid api

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,10 +5,15 @@ class HomeController < ApplicationController
     end
   
     def receive_data
-          data = request.body.receive_data
-          JSON.parse(data)
-          data['Countries'].each do |hash| CoronaDatum.find_by(country: hash['Country']).update(confirmed_cases: hash['TotalConfirmed']) end
-              
+        data = request.body.read
+        if data.present?
+            JSON.parse(data)
+            data['Countries'].each do |hash| CoronaDatum.find_by(country: hash['Country']).update(confirmed_cases: hash['TotalConfirmed']) end
+            render :json => {:status => 200}   
+        else
+            render :json => {:status => 404} 
+        end
+
     end
   
    
