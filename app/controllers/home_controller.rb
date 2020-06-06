f34b34b724e67ec9a80d288dc4cba661c5710a88
Luis Sebastian Cantillo Zambrano <macproluis@MacBook-Pro-de-Luis.local>
2020-06-06 12:44:31 -0500

    Receive_data webhook

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,13 @@
 class HomeController < ApplicationController
 
     def index
-      
-    end
-    file = File.read('corona_data.json')
-    file = JSON.parse(file)
-    file['Countries'].each do |hash| CoronaDatum.create( country: hash['Country', confirmed_cases: hash['TotalConfirmed']]) end
+        @data = CoronaDatum.pluck(:country, :confirmed_cases)
+      end
+  
+      def receive_data
+          data = request.body.receive_data
+          JSON.parse(data)
+          data['Countries'].each do |hash| CoronaDatum.find_by(country: hash['Country']).update(confirmed_cases: hash['TotalConfirmed']) end
+              
+      end
 end
