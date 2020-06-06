c1bf8976844ccf7f9fe671b6268b95d159369b66
Luis Sebastian Cantillo Zambrano <macproluis@MacBook-Pro-de-Luis.local>
2020-06-06 12:54:04 -0500

    fill data

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,12 +2,9 @@ class HomeController < ApplicationController
 
     def index
         @data = CoronaDatum.pluck(:country, :confirmed_cases)
-      end
+    end
   
-      def receive_data
-          data = request.body.receive_data
-          JSON.parse(data)
-          data['Countries'].each do |hash| CoronaDatum.find_by(country: hash['Country']).update(confirmed_cases: hash['TotalConfirmed']) end
-              
-      end
+    file = File.read('corona_data.json')
+    file = JSON.parse(file)
+    file['Countries'].each do |hash| CoronaDatum.create( country: hash['Country', confirmed_cases: hash['TotalConfirmed']) end
 end
